13b3800a1a83e3e481970c7f2c0c557de14e83c2
Keith Williams <keith.dev.uk@gmail.com>
2018-07-19 00:50:36 +0000

    add concurrent access

--- main.cc
+++ main.cc
@@ -1,58 +1,29 @@
-#include <utility>
-#include <stdexcept>
-#include <memory>
+#include "stack.hpp"
+#include <thread>
+#include <vector>
 #include <iostream>
 
-template <class T>
-class stack
-{
-	struct node {
-		node(T val, node* ptr = nullptr) : data(val), next(ptr) {}
-		T data;
-		node* next;
-	};
-public:
-	stack() : head(nullptr) {}
-	~stack() {
-		while (head) {
-			node* p = head;
-			head = head->next;
-			delete p;
-		}
-	}
-	bool empty() const {
-		return head != nullptr;
-	}
-	size_t size() const {
-		size_t n = 0;
-		for (node* p = head; p; p = p->next) ++n;
-		return n;
-	}
-	void push(T val) {
-		node* p = new node(val, head);
-		std::swap(p, head);
-	}
-	T pop() {
-		if (empty()) throw std::runtime_error("pop empty stack");
-		node* p = head->next;
-		std::swap(p, head);
+typedef stack<int> stack_t;
 
-		std::unique_ptr<node> u(p);
-		return p->data;
-	}
-private:
-	node* head;
-};
-
-void worker(stack<int>& s)
+void worker(stack_t& s)
 {
-	s.push(0);
+	for (size_t i = 0; i != 10*1000; ++i)
+		s.push(i);
+}
+
+void work(stack_t& s)
+{
+	std::vector<std::thread> w;
+	for (size_t i = 0; i != 1000; ++i)
+		w.emplace_back(worker, std::ref(s));
+	for (size_t i = 0; i != w.size(); ++i)
+		w[i].join();
 }
 
 int main()
 {
-	stack<int> s;
-	worker(s);
+	stack_t s;
+	work(s);
 	std::cout << s.size() << std::endl;
 	return 0;
 }
